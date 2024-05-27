4250632ecbca60b7f533dd898fc48a5bbe6238aa
KennyD4Christ <kennyd4christ@gmail.com>
2024-05-27 19:37:06 +0100

    models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -59,7 +59,7 @@ class BaseModel:
         models.storage.new(self)
         models.storage.save()
 
-    def to_dict(self, save_to_disk=False):
+    def to_dict(self):
         """returns a dictionary containing all keys/values of the instance"""
         new_dict = self.__dict__.copy()
         if "created_at" in new_dict:
@@ -69,8 +69,7 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if not save_to_disk:
-            new_dict.pop('password', None)
+        new_dict.pop('password', None)
         return new_dict
 
     def delete(self):

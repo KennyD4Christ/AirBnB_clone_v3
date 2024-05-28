aba797d04c099d3f43f9a7f6c6c1ba4643c18f66
KennyD4Christ <kennyd4christ@gmail.com>
2024-05-28 17:12:10 +0100

    api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,21 +15,3 @@ def status():
         JSON: A JSON object with the status.
     """
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def stats():
-    """
-    Route to return the number of each object type.
-    Returns:
-        JSON: A JSON object with the counts of each object type.
-    """
-    counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(counts)
